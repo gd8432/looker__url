2f5bdb389b0742f8ac82c52e74dee8e7b35c2cbf
Dev-98 <devparker48@gmail.com>
2023-12-26 14:07:38 +0530

    updates

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ def main():
 @app.route('/looker', methods=['GET'])
 def test_api():
     # Get parameters from the URL
-    External_user_id = request.args.get('External_user_id')
-    first_name = request.args.get('first_name')
-    last_name = request.args.get('last_name')
+    external_user_id = request.args.get('email')
+    first_name = request.args.get('name')
+    last_name = request.args.get('surname')
     
     # Check if all required parameters are provided
-    if not External_user_id or not first_name or not last_name:
+    if not external_user_id or not first_name or not last_name:
         return jsonify({"error": "Missing parameters"}), 500
 
-    result = looker_url(External_user_id,first_name,last_name)
+    try :
+        result = looker_url(external_user_id,first_name,last_name)
 
-    return jsonify({"result": result}), 200
+        return jsonify({"result": result}), 200
+    
+    except Exception as e:
+    
+        return jsonify({"error": e}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
